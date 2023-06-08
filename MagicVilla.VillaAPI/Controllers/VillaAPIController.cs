a3ed711fed061a18e9c09438751e2b652d19dfd3
srkans <serkansacma@hotmail.com>
2023-06-08 11:00:58 +0300

    controller, model, dto

---
+++ MagicVilla.VillaAPI/Controllers/VillaAPIController.cs
@@ -0,0 +1,36 @@
+﻿using MagicVilla.VillaAPI.Data;
+using MagicVilla.VillaAPI.Models;
+using MagicVilla.VillaAPI.Models.DTO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla.VillaAPI.Controllers
+{
+    [ApiController]
+    [Route("api/VillaAPI")]
+    public class VillaAPIController : ControllerBase
+    {
+        [HttpGet("[action]")]
+        public ActionResult<IEnumerable<VillaDTO>> GetVillas()
+        {
+            return Ok(VillaStore.villaList);
+        }
+
+        [HttpGet("[action]/{id:int}")]
+        public ActionResult<VillaDTO> GetVilla(int id)
+        {
+            if(id == 0)
+            {
+                return BadRequest();
+            }
+
+            VillaDTO villa = VillaStore.villaList.FirstOrDefault(temp => temp.Id == id);
+
+            if(villa == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(villa);
+        }
+    }
+}
---
+++ MagicVilla.VillaAPI/Data/VillaStore.cs
@@ -0,0 +1,13 @@
+﻿using MagicVilla.VillaAPI.Models.DTO;
+
+namespace MagicVilla.VillaAPI.Data
+{
+    public static class VillaStore
+    {
+        public static List<VillaDTO> villaList = new List<VillaDTO>()
+            {
+                new VillaDTO() {Id =1 , Name="Pool View"},
+                new VillaDTO() {Id =2 , Name="Beach View"}
+            };
+    }
+}
