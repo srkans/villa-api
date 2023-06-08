0114159286cdf04d45527a9d695c3e6974f77482
srkans <serkansacma@hotmail.com>
2023-06-08 14:35:29 +0300

    Serilog

--- MagicVilla.VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla.VillaAPI/Controllers/VillaAPIController.cs
@@ -10,10 +10,18 @@ namespace MagicVilla.VillaAPI.Controllers
     [Route("api/VillaAPI")]
     public class VillaAPIController : ControllerBase
     {
+        private readonly ILogger<VillaAPIController> _logger;
+
+        public VillaAPIController(ILogger<VillaAPIController> logger)
+        {
+            _logger = logger;
+        }
+
         [HttpGet("[action]")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         public ActionResult<IEnumerable<VillaDTO>> GetVillas()
         {
+            _logger.LogInformation("Getting all villas");
             return Ok(VillaStore.villaList);
         }
 
@@ -25,6 +33,7 @@ namespace MagicVilla.VillaAPI.Controllers
         {
             if(id == 0)
             {
+                _logger.LogError("Get villa error with id : " + id);
                 return BadRequest();
             }
 
