db83b1baabf20ccc3ccc70b93083e34ebc3be7b3
srkans <serkansacma@hotmail.com>
2023-06-12 09:36:29 +0300

    http service

--- MagicVilla.Web/Services/IServices/IBaseService.cs
+++ MagicVilla.Web/Services/IServices/IBaseService.cs
@@ -1,6 +1,10 @@
-﻿namespace MagicVilla.Web.Services.IServices
+﻿using MagicVilla.Web.Models;
+
+namespace MagicVilla.Web.Services.IServices
 {
     public interface IBaseService
     {
+        APIResponse responseModel { get; set; }
+        Task<T> SendAsync<T>(APIRequest apiRequest);
     }
 }
---
+++ MagicVilla.Web/Services/VillaService.cs
@@ -0,0 +1,65 @@
+﻿using MagicVilla.Web.Models.DTO;
+using MagicVilla.Web.Services.IServices;
+using MagicVilla.Web.Models;
+using MagicVilla.Utility;
+
+namespace MagicVilla.Web.Services
+{
+    public class VillaService : BaseService, IVillaService
+    {
+        private readonly IHttpClientFactory _clientFactory;
+        private string _villaUrl;
+        public VillaService(IHttpClientFactory httpClientFactory,IConfiguration configuration) : base(httpClientFactory)
+        {
+            _clientFactory = httpClientFactory;
+            _villaUrl = configuration.GetValue<string>("ServiceUrls:VillaAPI");
+        }
+
+        public Task<T> CreateAsync<T>(VillaCreateDTO dto)
+        {
+            return SendAsync<T>(new APIRequest() 
+            { 
+                ApiType = StaticDetails.ApiType.POST,
+                Data = dto,
+                Url = _villaUrl+"/api/VillaAPI"+ "/CreateVilla"
+            });
+        }
+
+        public Task<T> DeleteAsync<T>(int id)
+        {
+            return SendAsync<T>(new APIRequest()
+            {
+                ApiType = StaticDetails.ApiType.DELETE,
+                Url = _villaUrl + "/api/VillaAPI" + "/DeleteVilla/"+id
+            });
+        }
+
+        public Task<T> GetAllAsync<T>()
+        {
+            return SendAsync<T>(new APIRequest()
+            {
+                ApiType = StaticDetails.ApiType.GET,
+                Url = _villaUrl + "/api/VillaAPI" + "/GetVillas"
+            });
+        }
+
+        public Task<T> GetAsync<T>(int id)
+        {
+            return SendAsync<T>(new APIRequest()
+            {
+                ApiType = StaticDetails.ApiType.GET,
+                Url = _villaUrl + "/api/VillaAPI" + "/GetVilla/" + id
+            });
+        }
+
+        public Task<T> UpdateAsync<T>(VillaUpdateDTO dto)
+        {
+            return SendAsync<T>(new APIRequest()
+            {
+                ApiType = StaticDetails.ApiType.PUT,
+                Data = dto,
+                Url = _villaUrl + "/api/VillaAPI" + "/UpdateVilla/" + dto.Id
+            });
+        }
+    }
+}
